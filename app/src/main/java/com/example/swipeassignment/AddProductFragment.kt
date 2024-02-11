24701a8901712403fb39c32b33bf34c5f956c76f
p4puniya <gauravpny@gmail.com>
2024-02-11 12:07:01 +0530

    bottomSheetAdded

---
+++ app/src/main/java/com/example/swipeassignment/AddProductFragment.kt
@@ -0,0 +1,19 @@
+package com.example.swipeassignment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class AddProductFragment : BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_product, container, false)
+    }
+
+}
